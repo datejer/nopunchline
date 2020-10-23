96828dd6568e29e01e99461b101f14a671adc037
datejer <ejer.priv@wp.pl>
2020-10-23 12:03:45 +0200

    Add only punchline route

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
+import Home from "./Home.vue";
+import Punchline from "./Punchline.vue";
 import "./assets/global.css";
+
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
+const routes = [
+	{ path: "/", component: Home },
+	{ path: "/punchline", component: Punchline },
+];
+
+const router = new VueRouter({
+	mode: "history",
+	routes,
+});
+
 new Vue({
+	router,
 	render: (h) => h(App),
 }).$mount("#app");
